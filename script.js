6d58ffd9013da034059497a1f503b35520a65685
fardin-rahman-sojon <sojoneditz25@gmail.com>
2025-08-24 00:01:51 +0600

    home update

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ document.getElementById('btn-login')
 .addEventListener("click", function(event){
     event.preventDefault()
     const mobileNumber = 1650230536;
-    const pinNumber = 2123;
+    const pinNumber = 1234;
     const mobileNumberValue = document.getElementById('mobile-number').value
     const mobileNumberValueConverted = parseInt(mobileNumberValue);
 
