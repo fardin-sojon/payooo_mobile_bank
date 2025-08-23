84de2095932a2c309cdc2634b1ba59d0111b42b3
fardin-rahman-sojon <sojoneditz25@gmail.com>
2025-08-23 21:39:52 +0600

    first commit and added login section

---
+++ script.js
@@ -0,0 +1,25 @@
+console.log('Connected Script');
+
+// Login Button funstionality
+
+document.getElementById('btn-login')
+.addEventListener("click", function(event){
+    event.preventDefault()
+    const mobileNumber = 1650230536;
+    const pinNumber = 2123;
+    const mobileNumberValue = document.getElementById('mobile-number').value
+    const mobileNumberValueConverted = parseInt(mobileNumberValue);
+
+    const pinNumberValue = document.getElementById('pin-number').value
+    const pinNumberValueConverted = parseInt(pinNumberValue);
+
+    console.log(mobileNumberValueConverted, pinNumberValueConverted);
+    
+
+    if(mobileNumberValueConverted === mobileNumber && pinNumberValueConverted === pinNumber){
+        window.location.href="./home.html"
+    }
+    else{
+        alert('Number Or Pin Invalid,,Please Inter Currect Pin Or Number')
+    }
+})
