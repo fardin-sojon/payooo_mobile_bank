c4aea7cc96fb7e9b4ebe5466e5e794cf1cc57098
fardin-rahman-sojon <sojoneditz25@gmail.com>
2025-08-25 17:41:03 +0600

    toggling feature added

--- home.js
+++ home.js
@@ -1,21 +1,54 @@
 let validPin = 1234;
 
 // function to get input values
-    function getInputValueNumber(id){
-        const inputField = document.getElementById(id)
-        const inputFieldValue = inputField.value;
-        const inputFieldValueNumber = parseInt(inputFieldValue);
-        
-        return inputFieldValueNumber;
-        
-    }
-    function getInputValue(id){
-        const inputField = document.getElementById(id)
-        const inputFieldValue = inputField.value;
+function getInputValueNumber(id) {
+    const inputField = document.getElementById(id)
+    const inputFieldValue = inputField.value;
+    const inputFieldValueNumber = parseInt(inputFieldValue);
 
-        return inputFieldValue;
-    }
+    return inputFieldValueNumber;
 
+}
+// function to get value
+function getInputValue(id) {
+    const inputField = document.getElementById(id)
+    const inputFieldValue = inputField.value;
+
+    return inputFieldValue;
+}
+// functions to get innerText
+function getInputInnerText(id) {
+    const inputField = document.getElementById(id)
+    const inputFieldInnerText = inputField.innerText;
+    const inputInnerTextValue = parseInt(inputFieldInnerText)
+
+    return inputInnerTextValue;
+}
+// function to set InnerText
+function setInnerText(value) {
+    const availableBalenceElament = document.getElementById("available-balance");
+    availableBalenceElament.innerText = value;
+}
+// function to toggle
+function handleToggle(id) {
+    const forms = document.getElementsByClassName("form")
+    for (const from of forms) {
+        from.style.display = "none"
+        document.getElementById(id).style.display = "block"
+    }
+}
+// function to toggle Buttons
+    function handleToggleButton(id){
+        const formButtons = document.getElementsByClassName("form-btn")
+
+        for(const btn of formButtons){
+            btn.classList.remove("border-[#0874f2]", "bg-[#0874f20d]")
+            btn.classList.add("border-gray-200")
+        }
+
+        document.getElementById(id).classList.remove("border-gray-200")
+        document.getElementById(id).classList.add("border-[#0874f2]", "bg-[#0874f20d]")
+    }
 // Add money feature
 document.getElementById("btn-add-money")
     .addEventListener("click", function (event) {
@@ -27,7 +60,7 @@ document.getElementById("btn-add-money")
 
         const pin = getInputValueNumber("add-pin")
 
-        const availableBalence = parseInt(document.getElementById("available-balance").innerText)
+        const availableBalence = getInputInnerText("available-balance")
 
         if (accountNumber.length < 11) {
             alert("Please provide valid Accouont Number")
@@ -40,7 +73,8 @@ document.getElementById("btn-add-money")
         }
 
         const totalNewAvaibleBalance = amount + availableBalence
-        document.getElementById("available-balance").innerText = totalNewAvaibleBalance
+
+        setInnerText(totalNewAvaibleBalance)
     })
 // Cash Out feature
 document.getElementById("btn-withdraw-money")
@@ -51,7 +85,7 @@ document.getElementById("btn-withdraw-money")
         const amount = getInputValueNumber("widhdraw-amount")
 
         const cashPin = getInputValueNumber("cash-out-pin")
-        
+
 
         if (agentNumber.length < 11) {
             alert("Please Provide valid Agent Number")
@@ -62,25 +96,70 @@ document.getElementById("btn-withdraw-money")
             return;
         }
 
-        const availableBalence = parseInt(document.getElementById("available-balance").innerText)
+        const availableBalence = getInputInnerText("available-balance")
 
 
         const totalNewAvaibleBalance = availableBalence - amount
         console.log(totalNewAvaibleBalance)
 
-        document.getElementById("available-balance").innerText = totalNewAvaibleBalance
+        setInnerText(totalNewAvaibleBalance);
     })
 
 // toggling feature
 
 document.getElementById("add-button")
     .addEventListener("click", function () {
-        document.getElementById("cash-out-parent").style.display = "none"
-        document.getElementById("add-money-parent").style.display = "block"
+        // document.getElementById("cash-out-parent").style.display = "none"
+        // document.getElementById("transfer-money-parent").style.display = "none"
+        // document.getElementById("get-bonus-parent").style.display = "none"
+        // document.getElementById("add-money-parent").style.display = "block"
+
+        // const forms = document.getElementsByClassName("form")
+        // for (const from of forms) {
+        //     from.style.display = "none"
+        //     document.getElementById("add-money-parent").style.display = "block"
+        // }
+        handleToggle("add-money-parent")
+
+        handleToggleButton("add-button")
     })
 
 document.getElementById("cash-out-button")
     .addEventListener("click", function () {
-        document.getElementById("add-money-parent").style.display = "none"
-        document.getElementById("cash-out-parent").style.display = "block"
+
+        handleToggle("cash-out-parent")
+
+        handleToggleButton("cash-out-button")
+    })
+
+document.getElementById("transfer-money-button")
+    .addEventListener("click", function () {
+
+        handleToggle("transfer-money-parent")
+
+        handleToggleButton("transfer-money-button")
+    })
+
+document.getElementById("get-bonus-button")
+    .addEventListener("click", function () {
+
+        handleToggle("get-bonus-parent")
+
+        handleToggleButton("get-bonus-button")
+    })
+
+document.getElementById("pay-bill-button")
+    .addEventListener("click", function () {
+
+        handleToggle("pay-bill-parent")
+
+        handleToggleButton("pay-bill-button")
+    })
+
+document.getElementById("transactions-button")
+    .addEventListener("click", function () {
+
+        handleToggle("transactions-parent")
+
+        handleToggleButton("transactions-button")
     })
